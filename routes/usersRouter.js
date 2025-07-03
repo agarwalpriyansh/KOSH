66b878055baf9c0a9fab9e65bb6820811ca2fa8a
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-07-03 11:34:29 +0530

    fixes

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Owners Home Page');
+    res.send('users page');
 }); 
 
 module.exports = router;
