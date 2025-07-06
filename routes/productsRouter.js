606de1d651ddc9718bd1c4d82cf3c4a4de6c1e56
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-07-06 12:32:53 +0530

    set up create product route in product route  to upload product using multer

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../config/multer-config');
+const productModel = require('../models/product-model');
 
-router.get('/', (req, res) => {
-    res.send('products page');
+router.post('/create', upload.single("image"),async(req, res) => {
+    const product=await productModel.create({
+        image: req.file.buffer,
+        name: req.body.name,
+        price: req.body.price,
+        discount: req.body.discount || 0,
+        bgColor: req.body.bgColor,
+        panelColor: req.body.panelColor,
+        textColor: req.body.textColor
+    })
 }); 
 
 module.exports = router;
